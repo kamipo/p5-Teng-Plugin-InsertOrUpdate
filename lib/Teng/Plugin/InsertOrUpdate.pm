1ba82d51f9a2a9a3b3e1acef9764240bb00e0b11
Ryuta Kamizono <kamipo@gmail.com>
2011-01-21 20:39:03 +0900

    first commit

--- lib/Teng/Plugin/InsertOrUpdate.pm
+++ lib/Teng/Plugin/InsertOrUpdate.pm
@@ -1,9 +1,41 @@
 package Teng::Plugin::InsertOrUpdate;
 use strict;
 use warnings;
-use 5.00800;
-our $VERSION = '0.01';
+use utf8;
 
+our @EXPORT = qw/insert_or_update/;
+
+sub insert_or_update {
+    my ($self, $table, $args, $update_args) = @_;
+
+    my $prefix = 'INSERT';
+    my $tbl = $self->schema->get_table($table);
+
+    for my $col (keys %{$args}) {
+        $args->{$col} = $tbl->call_deflate($col, $args->{$col});
+    }
+
+    my ($sql1, @binds1) = $self->sql_builder->insert( $table, $args, { prefix => $prefix } );
+
+    $update_args ||= [%$args];
+
+    for my $col (keys %{$update_args}) {
+        $update_args->{$col} = $tbl->call_deflate($col, $update_args->{$col});
+    }
+
+    my ($sql2, @binds2) = $self->sql_builder->update( $table, $update_args );
+    $sql2 =~ s/UPDATE\s(?:\S+)\sSET/ ON DUPLICATE KEY UPDATE/;
+
+    my $sth = $self->_execute($sql1.$sql2, [(@binds1, @binds2)], $table);
+
+    $tbl->row_class->new(
+        {
+            row_data   => $args,
+            teng       => $self,
+            table_name => $table,
+        }
+    );
+}
 
 
 1;
